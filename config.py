f3b1f7f55dfd1e6928a7f50a8f35f8d9cc61ea72
nandanakrish9 <nk3389@nyu.edu>
2026-05-30 14:52:42 -0400

    simplify config for deployment

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-
-
 import streamlit as st
+API_KEY = st.secrets["dace758b66514bcfb07d8b876f96afa1"]
 
-try:
-    API_KEY = st.secrets["dace758b66514bcfb07d8b876f96afa1"]
-except:
-    from dotenv import load_dotenv
-    import os
-    load_dotenv()
-    API_KEY = os.getenv("dace758b66514bcfb07d8b876f96afa1")
